e2a3e332499e30b8fec3fc63c1e90b5459bbf6f0
HARSH KHARE <harshkhare7160@gmail.com>
2023-08-01 07:32:48 +0530

    completed

--- Task Manager/script.js
+++ Task Manager/script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-Box");
-const  listContainer = document.getElementById("list-container")
+const  listContainer = document.getElementById("list-container");
+
 function addTask(){
     if(inputBox.value ==''){
         alert("Must write something");
@@ -7,6 +8,29 @@ function addTask(){
     else{
         let li= document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appearChild(li);
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML ="\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData()
 }
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
